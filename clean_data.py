acbb29cd052dbde3845beed0f42bf3cbf214caf1
GdulskiJ <s27753@pjwstk.edu.pl>
2025-10-09 23:34:18 +0200

    Dodanie skryptu czyszczącego dane i loggera

---
+++ clean_data.py
@@ -0,0 +1,64 @@
+import logging
+
+import pandas as pd
+logging.basicConfig(
+    level=logging.INFO,  # poziom logowania
+    handlers=[
+        logging.FileHandler("log.txt", mode='w'),  # zapis do pliku
+    ]
+)
+logging.info("Rozpoczęcie przetwarzania danych")
+
+
+df = pd.read_csv("data_student_27753.csv")
+logging.info(f"Dane wczytane. Liczba wierszy: {df.shape[0]}, kolumn: {df.shape[1]}")
+
+print(df)
+print(df.columns)
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+print(df.shape)
+initial_rows = df.shape[0]
+df = df.dropna(subset=['Płeć'])
+removed_rows = initial_rows - df.shape[0]
+logging.info(f"Liczba usuniętych wierszy (brak płci): {removed_rows}")
+
+print(df.isnull().sum())
+nulls_before = df.isnull().sum()
+
+
+
+
+median_age = int(df['Wiek'].median())
+df['Wiek'] = df['Wiek'].fillna(median_age)
+
+df['Wykształcenie'] = df['Wykształcenie'].fillna('Średnie')
+
+
+mean_salary = df['Średnie Zarobki'].mean()
+df['Średnie Zarobki'] = df['Średnie Zarobki'].fillna(mean_salary)
+
+
+def fill_time_mean(column):
+    times = df[column].dropna().str.split(':', expand=True).astype(int)
+    mean_hour = int(times[0].mean())
+    mean_minute = int(times[1].mean())
+    df[column] = df[column].fillna(f"{mean_hour:02}:{mean_minute:02}")
+
+fill_time_mean('Czas Początkowy Podróży')
+fill_time_mean('Czas Końcowy Podróży')
+
+df['Cel Podróży'] = df['Cel Podróży'].fillna('Inne')
+
+
+
+
+print(df.shape)
+print(df.isnull().sum())
+nulls_after = df.isnull().sum()
+filled = nulls_before - nulls_after
+logging.info(f"Liczba wypełnionych danych w kolumnach: \n{filled}")
+
+logging.info(f"Liczba wierszy po czyszczeniu: {df.shape[0]}")
+logging.info("Zakończenie przetwarzania danych")
